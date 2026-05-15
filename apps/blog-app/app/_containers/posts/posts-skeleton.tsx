993c546c924382d8e1a394fccfdb3b0c91166fac
佐藤　昭文 <01047245@CF5231.local>
2026-05-15 23:02:58 +0900

    fix: key微修正とOpenAPI修正

--- apps/blog-app/app/_containers/posts/posts-skeleton.tsx
+++ apps/blog-app/app/_containers/posts/posts-skeleton.tsx
@@ -4,7 +4,7 @@ export function PostsSkeleton() {
   return (
     <div className="flex flex-col gap-6">
       <ul className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-        {[...Array(10)].map((i) => (
+        {[...Array(10)].map((_, i) => (
           <li
             key={`posts-skeleton-${i}`}
             className="animate-pulse p-4 border-b border-gray-200 grid grid-rows-[auto_1fr_auto] gap-3"
